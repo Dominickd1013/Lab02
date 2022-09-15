3cdea409c8dbed0c08fbbcd050459a94539531e0
Dominick <domenico.damato@vtsd.com>
2022-09-15 11:56:07 -0400

    Lab assignment

---
+++ Lab.java
@@ -0,0 +1,24 @@
+public class Lab {
+
+    public static void main(String[] args)
+        {
+            System.out.println();
+            System.out.println("Lab 02, 100 point version");
+            System.out.println();
+            System.out.println();
+            System.out.print("Measure today's knowledge");
+            System.out.println(" by yesterdays confusion.");
+            System.out.println();
+            System.out.println("Bewilderment + exposure = Obvious");
+            System.out.println("# #####  ##### ##### ##### # #   # ##### #####");
+            System.out.println("# #   #  #   # #   # #   # # ##  #   #   #");
+            System.out.println("# #   #  #   # ##### #   # # # # #   #   #####");
+            System.out.println("# #   #  #   # #     #   # # #  ##   #       #");
+            System.out.println("# #####  ##### #     ##### # #   #   #   #####");
+
+
+
+
+        }
+
+}
